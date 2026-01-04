4ea2c19ee8e6c0e18a821348540c1d22cd1dc2c0
Taylor Deakyne <taylor.deakyne@gmail.com>
2026-01-04 21:08:26 +0000

    File changes

--- src/pages/AppointmentDetail.jsx
+++ src/pages/AppointmentDetail.jsx
@@ -49,8 +49,8 @@ export default function AppointmentDetail() {
   const { data: horses = [] } = useQuery({
     queryKey: ['horses'],
     queryFn: async () => {
-      const response = await base44.functions.invoke('getMyData', { entity: 'Horse', query: {} });
-      return response.data.data;
+      const { data } = await base44.functions.invoke('getMyData', { entity: 'Horse', query: {} });
+      return data.data || [];
     },
     enabled: !!user,
   });
@@ -58,8 +58,8 @@ export default function AppointmentDetail() {
   const { data: treatments = [] } = useQuery({
     queryKey: ['treatments', appointmentId],
     queryFn: async () => {
-      const response = await base44.functions.invoke('getMyData', { entity: 'Treatment', query: { appointment_id: appointmentId } });
-      return response.data.data;
+      const { data } = await base44.functions.invoke('getMyData', { entity: 'Treatment', query: { appointment_id: appointmentId } });
+      return data.data || [];
     },
     enabled: !!appointmentId && !!user,
   });
